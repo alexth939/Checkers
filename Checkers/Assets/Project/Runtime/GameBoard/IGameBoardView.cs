c948f49dac2e97c544881c6429d1ab14d3e5654a
alexth939 <41789108+alexth939@users.noreply.github.com>
2023-05-27 15:42:28 +0300

    [Fix] Remove dublicate

--- Checkers/Assets/Project/Runtime/GameBoard/IGameBoardView.cs
+++ Checkers/Assets/Project/Runtime/GameBoard/IGameBoardView.cs
@@ -3,24 +3,16 @@ using UnityEngine;
 
 namespace Runtime.GameBoard
 {
-     internal interface IGameBoardView
-     {
-          Vector2 SpriteSize { get; }
-
-          void Init(GameBoardMath boardMath, GameBoardGenerators boardGenerators);
-          void ShowBoard();
-          void SpawnChecker(CheckerView view, CheckerModel model, BoardSide targetStash);
-          void GetWorldAnchors(out Vector3 minBoardWorldAnchor, out Vector3 maxBoardWorldAnchor);
-          void MoveChecker(CheckerView checker, byte boardDestination, Action onDone = null);
-          void HightlightFields(params byte[] rawPositions);
-          void DimHightlightedFields();
-     }
-﻿using UnityEngine;
-
-namespace Runtime.GameBoard
-{
-    public interface IGameBoardView
+    internal interface IGameBoardView
     {
-        void ShowBoard(Texture2D boardTexture);
+        Vector2 SpriteSize { get; }
+
+        void Init(GameBoardMath boardMath, GameBoardGenerators boardGenerators);
+        void ShowBoard();
+        void SpawnChecker(CheckerView view, CheckerModel model, BoardSide targetStash);
+        void GetWorldAnchors(out Vector3 minBoardWorldAnchor, out Vector3 maxBoardWorldAnchor);
+        void MoveChecker(CheckerView checker, byte boardDestination, Action onDone = null);
+        void HightlightFields(params byte[] rawPositions);
+        void DimHightlightedFields();
     }
 }
